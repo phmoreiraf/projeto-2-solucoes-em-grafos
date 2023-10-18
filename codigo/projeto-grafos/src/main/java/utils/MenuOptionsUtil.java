9ff3e4a1ecfe083e2e9aa680787c9f9a0d31f378
Lucas Soares <polonio230@hotmail.com>
2023-10-18 17:42:13 -0300

    Refactor: add static class MenuOptionUtil

---
+++ codigo/projeto-grafos/src/main/java/utils/MenuOptionsUtil.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class MenuOptionsUtil {
+    public static final String MENU_HEADER = "\n---";
+    public static final String MENU_FOOTER = "---";
+    public static final String MENU_OPTIONS =
+            "Selecione qual questão deseja verificar: (A, B, C, D / S para sair)\n" +
+                    "Letra A - Existe estrada de qualquer cidade para qualquer cidade?\n" +
+                    "Letra B - Cidades não alcançadas por uma determinada cidade:\n" +
+                    "Letra C - Recomendação de visitas: (Ex.: 0)\n" +
+                    "Letra D - Menores distâncias possíveis a partir de determinada cidade:";
+
+}
